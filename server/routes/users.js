677a30efa1d14abc2255ba782ec3cb6c3aa9d324
tangjie <1129451889@qq.com>
2017-10-08 08:54:43 +0800

    服务端

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+router.post("/login", function (req, res, next) {
+  var param = {
+    userName = req.body.userName,
+    userPwd = req.body.userPwd
+  }
+})
 
 module.exports = router;
